b3437ca89a604fa70935e5a327975a858bdccf81
s20003 <s20003@std.it-college.ac.jp>
2022-12-01 11:37:38 +0900

    日数追加

--- app/src/main/java/jp/ac/it_college/std/s20003/qrcodetest/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20003/qrcodetest/MainActivity.kt
@@ -16,10 +16,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var hex1: String
     private lateinit var hex2: String
 
-    private lateinit var perfectQR: String   // 完成形をここに入れる
+    private lateinit var completeQR: String   // 完成形をここに入れる
 
-    private var num: Int = 0
-    private var viewProduct: String = ""
+    private var num1: Int = 0
+    private var num3: Int = 0
+    private var productView: String = ""
 
     private var code1 = ""
     private var code2 = ""
@@ -79,8 +80,9 @@ class MainActivity : AppCompatActivity() {
                     } else {
                         code2 = resultQR
                         if (hex2.startsWith("1")) {
-                            perfectQR = code1 + code2
-                            search()
+                            completeQR = code1 + code2
+                            nameSearch()
+                            daysSearch()
                         }
                         Toast.makeText(this, "２つ目のQRコードを読み取りました", Toast.LENGTH_LONG).show()
                     }
@@ -91,8 +93,9 @@ class MainActivity : AppCompatActivity() {
                     } else {
                         code3 = resultQR
                         if (hex2.startsWith("2")) {
-                            perfectQR = code1 + code2 + code3
-                            search()
+                            completeQR = code1 + code2 + code3
+                            nameSearch()
+                            daysSearch()
                         }
                         Toast.makeText(this, "３つ目のQRコードを読み取りました", Toast.LENGTH_LONG).show()
                     }
@@ -103,8 +106,9 @@ class MainActivity : AppCompatActivity() {
                     } else {
                         code4 = resultQR
                         if (hex2.startsWith("3")) {
-                            perfectQR = code1 + code2 + code3 + code4
-                            search()
+                            completeQR = code1 + code2 + code3 + code4
+                            nameSearch()
+                            daysSearch()
                         }
                         Toast.makeText(this, "４つ目のQRコードを読み取りました", Toast.LENGTH_LONG).show()
                     }
@@ -115,8 +119,9 @@ class MainActivity : AppCompatActivity() {
                     } else {
                         code5 = resultQR
                         if (hex2.startsWith("4")) {
-                            perfectQR = code1 + code2 + code3 + code4 + code5
-                            search()
+                            completeQR = code1 + code2 + code3 + code4 + code5
+                            nameSearch()
+                            daysSearch()
                         }
                         Toast.makeText(this, "５つ目のQRコードを読み取りました", Toast.LENGTH_LONG).show()
                     }
@@ -126,52 +131,103 @@ class MainActivity : AppCompatActivity() {
             if (resultQR.substring(0, 4) != "JAHIS") {
                 Toast.makeText(this, "指定のQRコードを読み取ってください", Toast.LENGTH_LONG).show()
             } else {
-                search()
+                nameSearch()
+                daysSearch()
             }
         }
     }
 
     // 薬名検索部分
-    private fun search() {
+    private fun nameSearch() {
         var startPoint: Int
         var namePosition = -1
 
         // 指定のQRコード以外を読み取ったときの処理を書く
         for (i in 1 until 10) {
             startPoint = namePosition + 1
-            namePosition = perfectQR.indexOf("201,", startPoint)
+            namePosition = completeQR.indexOf("201,", startPoint)
             if (namePosition == -1) {
                 break
             }
-            num = i
-            val extractView = perfectQR.substring(namePosition + 6)
+            num1 = i
+            val extractView = completeQR.substring(namePosition + 6)
             val commaPosition = extractView.indexOf(",", 6)
             if (commaPosition != -1) {
-                viewProduct = extractView.substring(0, commaPosition)
+                productView = extractView.substring(0, commaPosition)
                 textInput()
             } else {
-                viewProduct = extractView.substring(0)
+                productView = extractView.substring(0)
                 textInput()
             }
         }
     }
 
+    private fun daysSearch() {
+        var startPoint: Int
+        var namePosition = -1
+        for (i in 1 until 10) {
+            startPoint = namePosition + 1
+            namePosition = completeQR.indexOf("301,", startPoint)
+            if (namePosition == -1) {
+                break
+            }
+            num3 = i
+            val extractView = completeQR.substring(namePosition + 6)
+            val commaPosition1 = extractView.indexOf(",")
+            val viewProduct = extractView.substring(0, commaPosition1 + 10)
+            val daysPosition = viewProduct.indexOf("日分")
+            if (daysPosition == -1) {
+                break
+            }
+            val daysProduct = viewProduct.substring(daysPosition - 3)
+            val commaPosition2 = daysProduct.indexOf(",")
+            val dateView = daysProduct.substring(0, commaPosition2)
+            productView = dateView
+            daysInput()
+        }
+    }
+
     private fun textInput() {
-        when (num) {
+        when (num1) {
             1 -> {
-                binding.textView1.text = viewProduct
+                binding.textView1.text = productView
             }
             2 -> {
-                binding.textView2.text = viewProduct
+                binding.textView2.text = productView
             }
             3 -> {
-                binding.textView3.text = viewProduct
+                binding.textView3.text = productView
             }
             4 -> {
-                binding.textView4.text = viewProduct
+                binding.textView4.text = productView
             }
             5 -> {
-                binding.textView5.text = viewProduct
+                binding.textView5.text = productView
+            }
+        }
+    }
+
+    private fun daysInput() {
+        val day = if (productView != "") {
+            "日分"
+        } else {
+            ""
+        }
+        when (num3) {
+            1 -> {
+                binding.daysView1.text = productView + day
+            }
+            2 -> {
+                binding.daysView1.text = productView + day
+            }
+            3 -> {
+                binding.daysView1.text = productView + day
+            }
+            4 -> {
+                binding.daysView1.text = productView + day
+            }
+            5 -> {
+                binding.daysView1.text = productView + day
             }
         }
     }
